1a582ebb8fac8a671a10bccbff74e93c2dfb4515
ShyambHauper <shyam.bhalodiya@hauper.com>
2023-07-04 17:13:58 +0530

    backed

---
+++ src/config.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URL:
+    process.env.MONGODB_URL
+    || 'mongodb+srv://shyambhalodiya61:IfoBv1s9tObX53RD@cluster0.m9d2cdp.mongodb.net/safar?retryWrites=true&w=majority',
+  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+  accessKeyId: process.env.accessKeyId || 'accessKeyId',
+  secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
+};
